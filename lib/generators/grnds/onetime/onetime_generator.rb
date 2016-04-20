4ac82b145deca2682126e43fbc018d82b7db1b98
Maximo Dominguez <maximo.dominguez@grandrounds.com>
2016-04-20 11:08:37 -0700

    Pulls name and email from gitconfig

--- lib/generators/grnds/onetime/onetime_generator.rb
+++ lib/generators/grnds/onetime/onetime_generator.rb
@@ -6,11 +6,19 @@ module Grnds
 
     def onetime_script
       @now = Time.now
+
       template 'onetime_script.rb.erb', "script/onetime/#{stringified_timestamp}_#{file_name}.rb"
     end
 
     def stringified_timestamp
       @now.strftime '%Y%m%d%H%M%S'
+    end
+
+    def owner_body
+      git_name = `git config user.name`.chomp
+      git_email = `git config user.email`.chomp
+
+      (git_name.to_s.empty? && git_email.to_s.empty?)? nil : "\"#{git_name} (#{git_email})\""
     end
   end
 end
